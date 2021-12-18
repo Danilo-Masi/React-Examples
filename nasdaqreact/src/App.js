594b4daa3252bbb10bcd17b4217300278464c279
Danilo Masi <danilomasi999@gmail.com>
2021-12-18 15:44:41 +0100

    React Examples

--- nasdaqreact/src/App.js
+++ nasdaqreact/src/App.js
@@ -1,15 +1,37 @@
+import { Component } from 'react/cjs/react.development';
 import './App.css';
 import Stock from './Stock';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Applicazione react</h1>
-        <Stock nome = "Apple" fondatore = "Steve Jobs" eta = {11} />
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {showMsg: false, maggiorenne: ''};
+  }
+
+  showMaggiorenne = (nome) => {
+    this.setState({maggiorenne: nome});
+  }
+
+  render() {
+    return (
+      <div className='App'> 
+        <header className='App-header'>
+          <Stock 
+            nome = 'Apple' 
+            fondatore = 'Jobs' 
+            eta = {17}
+            showEta = {this.showMaggiorenne} />
+          <Stock 
+            nome = 'Google' 
+            fondatore = 'Brin' 
+            eta = {12}
+            showEta = {this.showMaggiorenne} />
+            <p>Figli Maggiorenni: {this.state.maggiorenne}</p>
+        </header>
+      </div>
+    );
+  }
+
 }
 
 export default App;
--- nasdaqreact/src/Stock.js
+++ nasdaqreact/src/Stock.js
@@ -1,18 +1,50 @@
 import React, { Component } from "react";
 
 class Stock extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {eta: this.props.eta};
+    }
+
+    aggiornaStato = () => {
+        this.setState((state,props) => ({eta: this.state.eta + 1}));
+        if(this.state.eta >= 18) {
+            this.props.showEta(this.props.nome);
+        }
+    }
+
+    realTime = () => {
+        setInterval(() => this.aggiornaStato(), 1000);
+    }
+
     render() {
-        const {nome, fondatore, eta} = this.props;
+        const {nome, fondatore} = this.props;
         return (
             <div>
-                <h3>Azienda: {nome.toUpperCase()}</h3>
+                <h3>Azienda: {nome.toUpperCase()} - Eta: {this.state.eta}</h3>
                 <p>Fondatore: {fondatore}</p>
                 <p>
-                    { eta>18 ? 'Sono maggiorenne': 'Sono minorenne'}
+                    { this.state.eta >= 18 ? 'Sono maggiorenne': 'Sono minorenne'}
                 </p>
+                <button onClick={this.realTime}>Aggiorno</button>
             </div>
         );
+        
     }
 }
+
+// Utilizzato per fornire dei valori di default
+Stock.defaultProps = {
+    nome: 'ND',
+    fondatore: 'ND'
+}
+
+// Utilizzato per la convalida dei tipi di dati
+// Stock.protoType = {
+//     nome: ProtoTypes.string,
+//     fondatore: PropTypes.string,
+//     eta: PropTypes.number
+// }
 
 export default Stock;
